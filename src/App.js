74c3dd8701ed7c57b4bd622d3bf70551a3bb6b5f
divinediscipline <divinediscipline4real@gmail.com>
2019-11-27 05:50:21 +0100

    add collection items to the shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
